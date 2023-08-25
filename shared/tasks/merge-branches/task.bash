eccf58b43869c1a0f6d7efe32f4ac4be78999196
Brandon Roberson <broberson@vmware.com>
2023-08-25 16:33:02 +0000

    WIP
    Signed-off-by: Amin Jamali <ajamali@vmware.com>

--- shared/tasks/merge-branches/task.bash
+++ shared/tasks/merge-branches/task.bash
@@ -2,22 +2,25 @@
 
 set -eu
 set -o pipefail
-set -x
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$THIS_FILE_DIR/../../../shared/helpers/helpers.bash"
 unset THIS_FILE_DIR
 init_git_author
 
-git config --global --add safe.directory '*'
-local ONTO_BRANCH_NAME="$(git -C ./onto-branch rev-parse --abbrev-ref HEAD)"
-local SOURCE_BRANCH_NAME="$(git -C ./source-branch rev-parse --abbrev-ref HEAD)"
-git clone ./source-branch ./merged-branch
+function run(){
+    git config --global --add safe.directory '*'
+    local onto_branch_name="$(git -C ./onto-branch rev-parse --abbrev-ref HEAD)"
+    local source_branch_name="$(git -C ./source-branch rev-parse --abbrev-ref HEAD)"
+    git clone ./source-branch ./merged-branch
 
-cd merged-branch
+    cd merged-branch
 
-git remote add local ../onto-branch
-git fetch local
-git checkout "local/${ONTO_BRANCH_NAME}"
+    git remote add local ../onto-branch
+    git fetch local
+    git checkout "local/${onto_branch_name}"
 
-git merge --no-edit "${SOURCE_BRANCH_NAME}"
+    git merge --no-edit "${source_branch_name}"
+}
+
+run
